a8eaab853e14d30d7bb1a4b52c630322877e66e0
Xhoni Filo <xhonifilo@Xhonis-MBP.attlocal.net>
2021-05-29 16:05:40 -0500

    Used class implementation for calculator

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
+class Calculator {
+    constructor() {
+        this.left = null;
+        this.right = null;
+        this.operator = null;
+        this.result = null;
 
-const calculator = document.querySelector(".calculator");
-const calculator_display = document.querySelector('.calculator-display div');
-const calculator_btns = document.querySelector(".calculator-btns");
+        this.display = document.querySelector('.calculator-display div');
+        this.btns = document.querySelector(".calculator-btns");
+    }
 
-calculator_btns.addEventListener("click", function (e) {
+    handleNumber(btn) {
+        if (calculator_display.textContent.length >= 23) {
+        }
+        else if (calculator_display.textContent == '0') {
+            calculator_display.textContent = btn.textContent;
+        }
+        else {
+            calculator_display.textContent += btn.textContent;
+        }
+    }
 
-    // Event Delegation
-    // code past this if statement will only run if its a cell that was clicked on.
-    if (e.target.matches('button')) {
-        const btn = e.target;
-        const btnContent = btn.textContent;
+    handleOperator(btn) {
+        btn.classList.add('is-depressed');
+    }
+
+    handleMisc(btn) {
+         // Determine which misc button was clicked
         const btnAction = btn.dataset.action;
-
-        // If any button was 'depressed', remove its depressed property and 
-        // save which button was 'depressed'
-        var btns = calculator_btns.getElementsByTagName('button');
-        var btn_depressed = null;
-        for (item of btns) {
-            if (item.classList.contains('is-depressed')) {
-                btn_depressed = item;
-                item.classList.remove('is-depressed');
-                break;
-            }
-        }
-
-        if (!btnAction) {
-            console.log('Number Key!');
-
-            if (calculator_display.textContent.length >= 23) {
-                // do nothing
-                // we don't want to display past our bounds
-            }
-
-            else if (calculator_display.textContent == '0') {
-                calculator_display.textContent = btn.textContent;
-            }
-
-            else {
-                calculator_display.textContent += btn.textContent;
-            }
-        }
-
-        if (btnAction == 'add' || btnAction == 'subtract' || btnAction == 'multiply' || btnAction == 'divide') {
-            console.log('Operator Key!');
-            
-            btn.classList.add('is-depressed');
-
-
-        }
 
         if (btnAction == 'decimal') {
             console.log('Decimal Key!');
@@ -70,16 +49,47 @@ calculator_btns.addEventListener("click", function (e) {
         if (btnAction == 'percent') {
             console.log('Percent Key!');
         }
+    }
+};
 
-        var value = parseFloat(calculator_display.textContent);
-        console.log(value);
+const calculator = new Calculator();
+
+calculator.btns.addEventListener("click", function (e) {
+
+    // Event Delegation
+    // code past this if statement will only run if its a cell that was clicked on.
+    if (e.target.matches('button')) {
+        const btn = e.target;
+
+        // If any button was 'depressed', remove its depressed property and 
+        // save which button was 'depressed'
+        var btns = calculator_btns.getElementsByTagName('button');
+        for (item of btns) {
+            if (item.classList.contains('is-depressed')) {
+                calculator.operator = item.textContent;
+                item.classList.remove('is-depressed');
+                break;
+            }
+        }
+
+
+        // Number button clicked
+        if (btn.classList.contains('btn-number')) {
+            console.log('Number Key!');
+            calculator.handleNumber(btn);
+        }
+
+        // Operator button clicked
+        if (btn.matches('btn-operator')) {
+            console.log('Operator Key!');
+            calculator.handleOperator(btn);
+        }
+
+        // Misc button clicked
+        if (btn.matches('btn-misc')) {
+            console.log('Misc Key!');
+            calculator.handleMisc(btn);
+        }
 
     }
-
-
-    // Grab text content
-    const val = e.target.textContent;
-
-
-
 });
