6e4f93054e338fae05bb2ec56278746be69639b5
Xhoni Filo <xhonifilo@Xhonis-MBP.attlocal.net>
2021-05-29 20:47:39 -0500

    Basic operations work now. Need to handle many edge cases; as well as  use new this.result for better coding practice.

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 class Calculator {
-    constructor() {
+    constructor({display, btns}) {
         this.left = null;
         this.right = null;
         this.operator = null;
         this.result = null;
-
-        this.display = document.querySelector('.calculator-display div');
-        this.btns = document.querySelector(".calculator-btns");
+        this.display = display;
+        this.btns = btns;
     }
 
     handleNumber(btn) {
-        if (calculator_display.textContent.length >= 23) {
+        // Handle display 
+        this.updateDisplay(btn);
+
+        if (this.operator == null) {
+            this.left = this.display.textContent;
         }
-        else if (calculator_display.textContent == '0') {
-            calculator_display.textContent = btn.textContent;
-        }
+
         else {
-            calculator_display.textContent += btn.textContent;
+            this.right = this.display.textContent;
         }
     }
 
     handleOperator(btn) {
+        // Add 'is-depressed' property to btn to change its css
         btn.classList.add('is-depressed');
+        // Save operator to be used for future operation
+        this.operator = btn.textContent;
     }
 
     handleMisc(btn) {
@@ -30,16 +34,22 @@ class Calculator {
 
         if (btnAction == 'decimal') {
             console.log('Decimal Key!');
-            calculator_display.textContent += '.';
+            this.display.textContent += '.';
         }
 
         if (btnAction == 'clear') {
             console.log('Clear Key!');
-            calculator_display.textContent = '0';
+            this.display.textContent = '0';
+            this.operator = null;
+            this.right = null;
         }
 
         if (btnAction == 'calculate') {
             console.log('Equals Key!');
+
+            if (this.right != null) {
+                this.display.textContent = eval(this.left + this.operator + this.right).toString();
+            }
         }
 
         if (btnAction == 'sign') {
@@ -50,28 +60,44 @@ class Calculator {
             console.log('Percent Key!');
         }
     }
+
+    updateDisplay(btn) {
+        // Handles case when text exceeds display box boundaries
+        if (this.display.textContent.length >= 23) {
+            // DO NOTHING
+        }
+        // Handles case when nothing has been entered in yet
+        else if (this.display.textContent == '0') {
+            this.display.textContent = btn.textContent;
+        }
+        // Handles case when typing a number after choosing an operator
+        else if (this.operator != null) {
+            this.display.textContent = btn.textContent;
+        }
+        // Handles case when something existed in the display
+        else {
+            this.display.textContent += btn.textContent;
+        }
+
+    }
 };
 
-const calculator = new Calculator();
+// Create new calculator object to house information
+const calculator = new Calculator({
+    display: document.querySelector('.calculator-display div'),
+    btns: document.querySelector(".calculator-btns")
+});
 
 calculator.btns.addEventListener("click", function (e) {
 
-    // Event Delegation
-    // code past this if statement will only run if its a cell that was clicked on.
     if (e.target.matches('button')) {
         const btn = e.target;
 
-        // If any button was 'depressed', remove its depressed property and 
-        // save which button was 'depressed'
-        var btns = calculator_btns.getElementsByTagName('button');
+        // If any button was 'depressed', remove its depressed property
+        var btns = calculator.btns.getElementsByTagName('button');
         for (item of btns) {
-            if (item.classList.contains('is-depressed')) {
-                calculator.operator = item.textContent;
-                item.classList.remove('is-depressed');
-                break;
-            }
+            item.classList.remove('is-depressed');
         }
-
 
         // Number button clicked
         if (btn.classList.contains('btn-number')) {
@@ -80,13 +106,13 @@ calculator.btns.addEventListener("click", function (e) {
         }
 
         // Operator button clicked
-        if (btn.matches('btn-operator')) {
+        if (btn.classList.contains('btn-operator')) {
             console.log('Operator Key!');
             calculator.handleOperator(btn);
         }
 
         // Misc button clicked
-        if (btn.matches('btn-misc')) {
+        if (btn.classList.contains('btn-misc')) {
             console.log('Misc Key!');
             calculator.handleMisc(btn);
         }
